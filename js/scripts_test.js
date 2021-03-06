676372b22387c8a2767597ab8447cb12ede313c5
BE3dARt <gianluca.imbiscuso@gmail.com>
2021-03-07 00:55:33 +0100

    First iteration of calcualting elevation offset by means of measuring incline and calculating 3D vectors is done. Finally...

--- js/scripts_test.js
+++ js/scripts_test.js
@@ -16,7 +16,7 @@ var factorx = (54613 + 1 / 3);
 var factory = (54613 + 1 / 3);
 L.CRS.pr = L.extend({}, L.CRS.Simple, {
     projection: L.Projection.LonLat,
-    
+
     // Changing the transformation is the key part, everything else is the same.
     transformation: new L.Transformation(1 / factorx, 0, 1 / factory, 0),
 
@@ -72,10 +72,224 @@ function projectCoordinates(point) {
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Cross product
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function cross(A, B) {
+
+    return [A[1] * B[2] - A[2] * B[1], A[2] * B[0] - A[0] * B[2], A[0] * B[1] - A[1] * B[0]];
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Dot product
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function dot(A, B) {
+    return (A[0] * B[0] + A[2] * B[2] + A[1] * B[1]);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Multiply vector with scalar
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function multiply_vector_Scalar(vector, scalar) {
+    return [vector[0] * scalar, vector[1] * scalar, vector[2] * scalar];
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Add vector and vector
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function add_vector_vector(vector1, vector2) {
+    return [vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2]];
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Turn a given vector on the basis of an angle and the desired axis
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function rotate(inputvector, angle, axis) {
+
+    var resultingVector1 = multiply_vector_Scalar(inputvector, (Math.cos(angle)));
+    var resultingVector2 = multiply_vector_Scalar(cross(axis, inputvector), Math.sin(angle));
+    var resultingVector3 = multiply_vector_Scalar(multiply_vector_Scalar(axis, dot(axis, inputvector)), 1 - Math.cos(angle));
+    var result = add_vector_vector(add_vector_vector(resultingVector1, resultingVector2), resultingVector3);
+
+    return result;
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Return the normalized version of a vector. [Vector with length of 1]
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function magnitude(inputvector) {
+
+    return Math.sqrt(Math.pow(inputvector[0], 2) + Math.pow(inputvector[1], 2) + Math.pow(inputvector[2], 2));
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Return the normalized version of a vector. [Vector with length of 1]
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function normalize(inputvector) {
+
+    var length = Math.sqrt(Math.pow(inputvector[0], 2) + Math.pow(inputvector[1], 2) + Math.pow(inputvector[2], 2));
+    return [inputvector[0] / length, inputvector[1] / length, inputvector[2] / length];
+
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Calculate remaining direction
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function getDir(vector1, vector2, axis) {
+
+    if (axis == "top") {
+
+        var vec1 = normalize([vector1[0], 0, vector1[2]]);
+        var vec2 = normalize([vector2[0], 0, vector2[2]]);
+
+        return Math.acos(dot(vec1, vec2) / (magnitude(vec1) * magnitude(vec2))) / 2;
+
+    } else {
+
+        var vec1 = normalize(vector1);
+        var vec2 = normalize(vector2);
+
+        return Math.acos(dot(vec1, vec2) / (magnitude(vec1) * magnitude(vec2))) / 2;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Calculate elevation offset based on sourrounding incline
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function elevationOffset () {
-  //We need 4 points (corners)
+function elevationOffset(angle, elevation) {
+
+    var inputAngle = angle;
+    var angle = angle * (Math.PI / 180);
+
+    //Stich together target vector
+    elevation = elevation * (Math.PI / 180);
+    var targetVector = [Math.sin(elevation), Math.sin(elevation), 0];
+    targetVector = rotate(targetVector, -1 * angle, [0, 1, 0]);
+    console.log("Target Vector: " + targetVector);
+
+    //Calculate normalvectors of all 4 planes
+    //BEISPIEL: 5 Höhenabfragen = 1, 2, 3, 4, 5
+    //2, 3, 2, 1, 2
+    var origin = [0, 2, 0];
+    var normalVectorPlane1 = cross([-5, 3 - origin[1], 0], [0, 2 - origin[1], 5]); //Normalvector for upper-left corner
+    var normalVectorPlane2 = multiply_vector_Scalar(cross([5, 1 - origin[1], 0], [0, 2 - origin[1], 5]), -1); //Normalvector for upper-right corner
+    var normalVectorPlane3 = multiply_vector_Scalar(cross([-5, 3 - origin[1], 0], [0, 5 - origin[1], -5]), -1); //Normalvector for bottom-left corner
+    var normalVectorPlane4 = cross([5, 1 - origin[1], 0], [0, 5 - origin[1], -5]); //Normalvector for bottom-left corner
+
+    //Add the normalvectors together and produce a normalized normalvector of the center / This is the axis where the turret turns around
+    //var normalVectorTank = normalize(add_vector_vector(add_vector_vector(add_vector_vector(normalVectorPlane1, normalVectorPlane2), normalVectorPlane3), normalVectorPlane4));
+    var normalVectorTank = [0, 1, 0];
+
+    //Decide if to add or to remove from angle
+    var condition = "Plus";
+
+    //Start Vector; This remains the same always
+    var startVector = [1, 0, 0];
+
+    //Roted Vector
+    var modifiedVector = rotate(startVector, -1 * angle, normalVectorTank);
+
+    //Perpendicular Vector (Use 90° to calculate it)
+    var perpendicular = rotate(modifiedVector, -1 * 1.5707, normalVectorTank);
+
+    //Upwards rotated Vector (Bsp. 10° = 0.174), this is the displayed value in the tank!
+    var upwards = rotate(modifiedVector, 0.174, perpendicular);
+
+    console.log("Modified Vector: " + modifiedVector);
+    console.log("Perpendicular Vector: " + perpendicular);
+    console.log("Upward: " + upwards);
+
+    console.log("Elevation Check: " + getDir([0, 0, 1], upwards, "side") * 2 * 180 / Math.PI);
+
+    //It works until here!
+    //Now up to the difficult part... approximation
+    var correctionSideSum = 10;
+    var correctionTopSum = 0;
+
+    //1. We go directly to the target above
+    //2. With the upper part (The 10° incline) we need to figure out if we need to add angle or take back.
+    var correctionTop = getDir(upwards, targetVector, "top");
+    console.log("Correction from top: " + correctionTop * 180 / Math.PI);
+
+    //The problem is that it can't tell us if we should move left or right, the best thing would be to check real quick by trying
+    modifiedVector = rotate(upwards, correctionTop, normalVectorTank);
+    console.log("Corrected modified vector: " + modifiedVector);
+
+    //We need to get directions again, compare, and take action 
+    var newCorrectionTop = getDir(modifiedVector, targetVector, "top");
+    console.log("NEW Correction from top: " + correctionTop * 180 / Math.PI);
+
+    //We will need to reverse correctionTop in that case
+    if (newCorrectionTop > correctionTop) {
+        correctionTop = correctionTop * -1;
+        condition = "Minus";
+    } else {
+        condition = "Plus";
+    }
+
+    //We can add this to the total amount of angle
+    correctionTopSum += (correctionTop * 180 / Math.PI);
+
+    //Carry on with approximation
+    modifiedVector = rotate(upwards, correctionTop, normalVectorTank);
+    console.log("New Corrected modified vector: " + modifiedVector);
+
+    //Now we need to adjust elevation too
+    var correctionSide = getDir(modifiedVector, targetVector, "side");
+    console.log("Correction from side: " + (correctionSide * 180 / Math.PI));
+
+    console.log("End Elevation Check: " + getDir([0, 0, 1], modifiedVector, "side") * 2 * 180 / Math.PI);
+
+    //Adjust the elevation
+    upwards = rotate(modifiedVector, correctionSide, perpendicular);
+    console.log("Upwards: " + upwards);
+
+    console.log("End Elevation Check: " + getDir([0, 0, 1], upwards, "side") * 2 * 180 / Math.PI);
+
+    //We can add this to the total amount of elevation
+    correctionSideSum += (correctionSide * 180 / Math.PI);
+
+    counter = 0;
+    while (counter < 8) {
+
+        console.log("------------------------------------------------");
+
+        correctionTop = getDir(upwards, targetVector, "top");
+        //console.log("Correction from top: " + correctionTop * 180 / Math.PI);
+
+        //Adjust with knowledge from before
+        if (condition == "Minus") {
+            correctionTop = correctionTop * -1;
+        }
+
+        modifiedVector = rotate(upwards, correctionTop, normalVectorTank);
+        //console.log("Modified vector: " + modifiedVector);
+
+        //We can add this to the total amount of angle
+        correctionTopSum += (correctionTop * 180 / Math.PI);
+
+        correctionSide = getDir(modifiedVector, targetVector, "side");
+        console.log("Correction from side: " + correctionSide * 180 / Math.PI);
+
+        console.log("End Elevation Check: " + getDir([0, 0, 1], upwards, "side") * 2 * 180 / Math.PI);
+
+        upwards = rotate(modifiedVector, correctionSide, perpendicular);
+        console.log("Upwards: " + upwards);
+
+        console.log("End Elevation Check: " + getDir([0, 0, 1], upwards, "side") * 2 * 180 / Math.PI);
+
+        //We can add this to the total amount of elevation
+        correctionSideSum += (correctionSide * 180 / Math.PI);
+
+        counter += 1;
+
+    }
+
+    console.log("End Elevation: " + correctionSideSum);
+    console.log("End Elevation Check: " + getDir([0, 0, 1], upwards, "side") * 2 * 180 / Math.PI);
+    console.log("End Direction: " + (inputAngle + (correctionTopSum * -1)));
+
+
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -86,7 +300,7 @@ class Marking {
     constructor(position) {
         //Must be given as parameters
         this.position = position;
-        
+
         //Other variables
         this.firemode = "Close";
         this.distance = 0;
@@ -261,7 +475,7 @@ function addMarker(point, type, elevation, direction, firemode, counter) {
         closeOnClick: false,
         autoClose: false
     }).openPopup();
-    
+
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -272,7 +486,7 @@ function heightdataCallback(game, mapp, message, mode, markerCounter, start) {
     if (mode == "update") {
 
         var goal = markerArray.length;
-        
+
         //Go this path to update artillery when dragged
         if (markerCounter == 0) {
 
@@ -293,8 +507,8 @@ function heightdataCallback(game, mapp, message, mode, markerCounter, start) {
             }
 
             return;
-        
-        //Go this path to update target when independetly dragged
+
+            //Go this path to update target when independetly dragged
         } else {
 
             if (markerCounter != -9) {
@@ -304,7 +518,7 @@ function heightdataCallback(game, mapp, message, mode, markerCounter, start) {
             }
 
         }
-        
+
         if (markerArray[start] != null && start <= goal) {
 
             //Update it's positions
@@ -317,7 +531,7 @@ function heightdataCallback(game, mapp, message, mode, markerCounter, start) {
 
             //Populate popup content with the new data
             markerArray[start][0].setPopupContent(popupContent(markerArray[start][2].gunElevation, markerArray[start][2].direction, markerArray[start][2].firemode, "target")).openPopup();
-            
+
             //Enter recursion loop if artillery unit has been dragged and all target need updates
             if (markerArray[start + 1] != null && markerCounter == -9) {
                 requestHeight([markerArray[start + 1][2].position[0], markerArray[start + 1][2].position[1]], mapp, "", "update", -9, start + 1);
@@ -332,7 +546,7 @@ function heightdataCallback(game, mapp, message, mode, markerCounter, start) {
             if (markerArray[counter] == null) {
 
                 if (markerArray[0] == null) {
-                    
+
                     //Define marker type
                     var type = "artillery";
 
@@ -344,17 +558,17 @@ function heightdataCallback(game, mapp, message, mode, markerCounter, start) {
 
                     //Add the marker for the artillery unit
                     addMarker(mapp, type, [0, 0], 0, "None", counter);
-                    
+
                     //Adding circle to show minimal shooting distance
                     shootingBoundaries = L.circle([mapp[1], mapp[0]], {
                         radius: 826
                     }).addTo(map);
 
                 } else {
-                  
+
                     //Define marker type
                     var type = "target";
-                    
+
                     //Initialize new array parameters
                     markerArray[counter] = [0, type, new Marking([game[0], game[1], message])];
 
@@ -447,9 +661,9 @@ function onMapClick(e) {
 
 //Mouse events
 map.on('drag', function (e) {
-    
+
     console.log(markerArray);
-    
+
     map.panInsideBounds(bounds, {
         animate: false
     });
@@ -457,6 +671,7 @@ map.on('drag', function (e) {
 map.on('click', function (e) {
 
     //console.log(projectCoordinates([e.latlng.lng, e.latlng.lat]))
+    elevationOffset(90, 45);
 
 });
 map.on('contextmenu', onMapClick);
