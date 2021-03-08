33abbc25a9bb7daa84d6f160e95874b9bf0b9e42
BE3dARt <gianluca.imbiscuso@gmail.com>
2021-03-08 20:20:15 +0100

    Offset approximation reach a precise and solid state. I am very happy. The whole approximation part took about 25 hours to complete.

--- js/tiltOffset.js
+++ js/tiltOffset.js
@@ -59,6 +59,22 @@ function normalize(inputvector) {
     return [inputvector[0] / length, inputvector[1] / length, inputvector[2] / length];
 
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Display telemetry stuff of approximation function
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function display(message, data, type) {
+
+    if (type == "vector") {
+        console.log(message + ": v=(" + data[0].toFixed(2) + "," + data[2].toFixed(2) + "," + data[1].toFixed(2) + ")");
+    }
+
+    if (type == "scalar") {
+        console.log(message + ": " + data.toFixed(2));
+    }
+
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Calculate remaining direction
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -83,28 +99,14 @@ function getDir(vector1, vector2, axis) {
 
         //var vec1 = normalize([vector1[0], vector2[1], vector1[2]]);
         //var vec2 = normalize(vector2);
-        
-        var vec1 = vector1;
+
+        var vec1 = [vector2[0], vector1[1], vector2[2]];
         var vec2 = vector2;
 
         return Math.acos(dot(vec1, vec2) / (magnitude(vec1) * magnitude(vec2))) / 2;
     }
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Display telemetry stuff of approximation function
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function display(message, data, type) {
-
-    if (type == "vector") {
-        console.log(message + ": v=(" + data[0].toFixed(2) + "," + data[2].toFixed(2) + "," + data[1].toFixed(2) + ")");
-    }
-
-    if (type == "scalar") {
-        console.log(message + ": " + data.toFixed(2));
-    }
-
-}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Is used to receive height data
@@ -164,8 +166,7 @@ function gatherHeightData(inputangle, inputelevation, position) {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function elevationOffset(normalVectorTank) {
 
-    var printLog = true;
-
+    //Define some variables
     var inputAngle = angle;
     angle = angle * (Math.PI / 180);
 
@@ -173,159 +174,128 @@ function elevationOffset(normalVectorTank) {
     elevation = elevation * (Math.PI / 180);
     var targetVector = [Math.cos(elevation), Math.sin(elevation), 0];
     targetVector = rotate(targetVector, -1 * angle, [0, 1, 0]);
-
-
-    display("Start Target Vector", targetVector, "vector");
-
+    display("Target Vector", targetVector, "vector");
 
     //Calculate normalvectors of all 4 planes
-    //BEISPIEL: 5 Höhenabfragen = 1, 2, 3, 4, 5
-    //2, 3, 2, 1, 2
     var origin = [0, 0, 0];
     var normalVectorPlane1 = cross([-5, 3 - origin[1], 0], [0, 2 - origin[1], 5]); //Normalvector for upper-left corner
     var normalVectorPlane2 = multiply_vector_Scalar(cross([5, 1 - origin[1], 0], [0, 2 - origin[1], 5]), -1); //Normalvector for upper-right corner
     var normalVectorPlane3 = multiply_vector_Scalar(cross([-5, 3 - origin[1], 0], [0, 2 - origin[1], -5]), -1); //Normalvector for bottom-left corner
     var normalVectorPlane4 = cross([5, 1 - origin[1], 0], [0, 2 - origin[1], -5]); //Normalvector for bottom-left corner
-
-    //Add the normalvectors together and produce a normalized normalvector of the center / This is the axis where the turret turns around
-    //normalVectorTank = normalize(add_vector_vector(add_vector_vector(add_vector_vector(normalVectorPlane1, normalVectorPlane2), normalVectorPlane3), normalVectorPlane4));
-    //var normalVectorTank = [0, 1, 0];
-
-
     display("Normal Vector", normalVectorTank, "vector");
-
 
     //Decide if to add or to remove from angle
     var condition = "Plus";
 
     //Start Vector
-    var startVector = [1, 0, 0];
+    if (normalVectorTank[0] == 0) {
+        var startVector = [1, 0, 0];
+    } else {
+        var startVector = normalize([((-1 * normalVectorTank[1]) / normalVectorTank[0]), 1, 0]);
+    }
 
-    //display("Start Vector", startVector, "vector");
+    startVector[0] = Math.abs(startVector[0]);
+    normalize(startVector);
+    display("Start Vector", startVector, "vector");
 
-    //Roted Vector
+    //Rotate turret
+    var before;
     var modifiedVector = rotate(startVector, -1 * angle, normalVectorTank);
-    
-    var deviation = (90 - Math.acos(dot(modifiedVector, normalVectorTank) / (magnitude(modifiedVector) * magnitude(normalVectorTank))) * 180 / Math.PI);
+    var after = getDir(startVector, modifiedVector, "top") * 2;
 
-    console.log("Deviation: " + deviation);
-    
-    //Perpendicular Vector (Use 90° to calculate it)
+    //We can add this to the total amount of angle
+    var correctionTopSum = Math.abs(0 - after);
+
+    //Check if shooting is even possible
+    if (modifiedVector[1] > targetVector[1]) {
+        console.log("Angle too flat; Shooting not possible")
+        return;
+    }
+
+    //Elevate the turret
     var perpendicular = normalize(cross(modifiedVector, normalVectorTank));
+    var correctionSide = getDir(modifiedVector, targetVector, "side");
+    modifiedVector = rotate(modifiedVector, correctionSide, perpendicular);
 
-    //Upwards rotated Vector (Bsp. 10° = 0.174), this is the displayed value in the tank!
-    var upwards = rotate(modifiedVector, 0.174, perpendicular);
-
-    if (printLog) {
-        display("Perpendicular Vector", perpendicular, "vector");
-    }
-
-    if (printLog) {
-        console.log("------------------------------------------------");
-    }
-
-    if (printLog) {
-        display("Modified Vector", modifiedVector, "vector");
-        display("Upward Vector", upwards, "vector");
-    }
-
-    //It works until here!
-    //Now up to the difficult part... approximation
-    var correctionSideSum = 10 + deviation;
-    
-    //List 
-    //0.2 too far (bit uphill)
-    //0.5 too far (bit more uphill)
-    //0.4 too less (bit downhill)
-    //0.2 too less (bit more downhill)
-    
-    var correctionTopSum = 0;
-
-    //1. We go directly to the target above
-    //2. With the upper part (The 10° incline) we need to figure out if we need to add angle or take back.
-    var correctionTop = getDir(upwards, targetVector, "top");
-    var correctionSide = 0;
-    //console.log("Correction from top: " + correctionTop * 180 / Math.PI);
-
-    //The problem is that it can't tell us if we should move left or right, the best thing would be to check real quick by trying
-    modifiedVector = rotate(upwards, correctionTop, normalVectorTank);
-    //console.log("Corrected modified vector: " + modifiedVector);
-
-    //We need to get directions again, compare, and take action 
-    var newCorrectionTop = getDir(modifiedVector, targetVector, "top");
-    //console.log("NEW Correction from top: " + correctionTop * 180 / Math.PI);
-
-    //We will need to reverse correctionTop in that case
+    //Check to which side the turret should be turning
+    var correctionTop = getDir(modifiedVector, targetVector, "top");
+    var directionChecker = rotate(modifiedVector, correctionTop, normalVectorTank);
+    var newCorrectionTop = getDir(directionChecker, targetVector, "top");
     if (newCorrectionTop > correctionTop) {
         correctionTop = correctionTop * -1;
         condition = "Minus";
+        var correctionSideSum = correctionSide;
     } else {
         condition = "Plus";
+        var correctionSideSum = correctionSide * -1;
     }
+    
+    console.log(condition);
 
-    if (printLog) {
-        console.log("------------------------------------------------");
-        console.log("Loop->");
-    }
+    //This counts the 
+    
 
     var counter = 0;
     while (counter < 15) {
 
-        if (printLog) {
-            console.log("------------------------------------------------");
-        }
+        //Check which difference is bigger;
+        correctionTop = getDir(modifiedVector, targetVector, "top");
+        correctionSide = getDir(modifiedVector, targetVector, "side");
 
-        correctionTop = getDir(upwards, targetVector, "top");
-
-        //Adjust with knowledge from before
+        //Decide with knowledge from before
         if (condition == "Minus") {
             correctionTop = correctionTop * -1;
         }
 
-        modifiedVector = rotate(upwards, correctionTop, normalVectorTank);
+        //Always correct the one with a bigger difference
+        if (Math.abs(correctionTop) > Math.abs(correctionSide)) {
 
-        if (printLog) {
-            display("Correction Top", (correctionTop * 180 / Math.PI), "scalar");
-            display("Modified Vector", modifiedVector, "vector");
+            //Rotate turret
+            before = getDir(modifiedVector, targetVector, "top");
+            modifiedVector = rotate(modifiedVector, correctionTop, normalVectorTank);
+            after = getDir(modifiedVector, targetVector, "top");
+
+            //We can add this to the total amount of angle
+            correctionTopSum += Math.abs(before - after);
+            
+            display("Approximation TOP", modifiedVector, "vector");
+
+        } else {
+
+            //Calculate perpendicular vector (Used as axis when elevating turret)
+            perpendicular = normalize(cross(modifiedVector, normalVectorTank));
+
+            //Elevate turret
+            modifiedVector = rotate(modifiedVector, correctionSide, perpendicular);
+
+            //Add to the total amount of elevation
+            correctionSideSum += correctionSide;
         }
 
-        //We can add this to the total amount of angle
-        correctionTopSum += (correctionTop * 180 / Math.PI);
+        //Display the modified vector
+        display("Approximation Vector", modifiedVector, "vector");
 
-        correctionSide = getDir(modifiedVector, targetVector, "side");
-
-        perpendicular = normalize(cross(modifiedVector, normalVectorTank));
-
-        if (printLog) {
-            display("Perpendicular Vector", perpendicular, "vector");
+        //You don't have to calculate any further if this criteria is met:
+        if ((correctionTop * 180 / Math.PI) < 0.01 && (correctionSide * 180 / Math.PI) < 0.01) {
+            break;
         }
-
-        upwards = rotate(modifiedVector, correctionSide, perpendicular);
-        if (printLog) {
-            display("Correction Side", (correctionSide * 180 / Math.PI), "scalar");
-            display("Upward Vector", upwards, "vector");
-        }
-
-        //We can add this to the total amount of elevation
-        correctionSideSum += (correctionSide * 180 / Math.PI);
-
-        display("Correction Side SUM", correctionSideSum, "scalar");
 
         counter += 1;
 
     }
 
-    display("End Vector", upwards, "vector");
-    console.log("End Elevation: " + correctionSideSum);
-    console.log("Diviation from perfect elevation: " + (correctionSide * 180 / Math.PI));
-    var update = (inputAngle + (correctionTopSum * -1));
-    console.log("End Direction: " + transform(update, false));
-    console.log("Diviation from perfect direction: " + (correctionTop * 180 / Math.PI));
+    display("End Vector", modifiedVector, "vector"); //correct
+    console.log("End Elevation: " + (correctionSideSum * 180 / Math.PI)); //correct
 
+    console.log("Original Angle: " + transform(inputAngle, false));
+    console.log("Strife: " + transform((correctionTopSum * 180 / Math.PI), false));
+
+    console.log("Diviation from perfect direction: " + (correctionTop * 180 / Math.PI));
+    console.log("Diviation from perfect elevation: " + (correctionSide * 180 / Math.PI));
+
+    //Reset these variables to be used again later
     angle = NaN;
     elevation = NaN;
     heightData = [NaN, NaN, NaN, NaN];
-
-    return [correctionTopSum, correctionSideSum];
 
 }
